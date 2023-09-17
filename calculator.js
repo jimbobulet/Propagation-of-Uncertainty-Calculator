63df57cbbe9c55a12096b434f477dd6e8ce8deb8
jimbobulet <112141947+jimbobulet@users.noreply.github.com>
2023-09-17 13:27:56 +1200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function addition() {
+    let num1 = document.getElementById('Num1').value;
+    let num2 = document.getElementById('Num2').value;
+    let sum = +num1 + +num2;
+    alert(sum);
+    }
